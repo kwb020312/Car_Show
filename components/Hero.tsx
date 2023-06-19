31891cf740b5fd88fddae9838c5838daf54921c0
N S <kwb020312@naver.com>
2023-06-19 22:50:33 +0900

    Feat: 필요 이미지 추가

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  const handleScroll = () => {};
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          찾고, 예약하고, 쉽고 빠르게 차를 렌트하세요!
+        </h1>
+        <p className="hero__subtitle">
+          간편한 예약 프로세스를 통해 차량 렌트 시간을 절약할 수 있습니다.
+        </p>
+
+        <CustomButton
+          title="차량 조회하기"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
