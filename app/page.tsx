eef1bd536889937d5764f76f4b68afb041a835cb
N S <kwb020312@naver.com>
2023-06-20 23:22:39 +0900

    Feat: 자동차 검색 기능 생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
-import { Hero } from "@/components";
+import { CustomFilter, Hero, SearchBar } from "@/components";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">카탈로그</h1>
+          <p>마음에드는 차를 찾아보세요</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
